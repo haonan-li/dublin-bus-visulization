f74b17ec43842372010ad02e7d97519f9da57d9d
haonan-li <haonanl5@student.unimelb.edu.au>
2018-09-23 19:14:00 +1000

    modify route

--- src/data.py
+++ src/data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import json
 
 header = ['Timestamp','Line_ID','Direction','Journey_Pattern_ID','Time_Frame','Vehicle_Journey_ID', \
          'Operator','Congestion','Lon','Lat','Delay','Block_ID','Vehicle_ID','Stop_ID','At_Stop']
@@ -38,7 +39,7 @@ def congestion(data_file):
     ndf = keep_cols(df,cols)
     nndf = keep_rows(ndf,'Congestion',larger,1)
     output(nndf,'congestion/',data_file)
- 
+
 # Extract all stops
 def stops(data_file):
     df = pd.read_csv(data_dir + data_file, header=None, names=header)
@@ -47,29 +48,56 @@ def stops(data_file):
     nndf = keep_rows(ndf,'At_Stop',larger,1)
     output(nndf,'stop/',data_file)
 
+# Extract line's route
 def route(data_file):
     df = pd.read_csv(data_dir + data_file, header=None, names=header)
-    cols = ['Timestamp','Line_ID','Lon','Lat']
+    cols = ['Timestamp','Line_ID','Vehicle_ID','Lon','Lat']
     ndf = keep_cols(df,cols)
     nndf = ndf[~ndf.Line_ID.isnull()]
     line_ids = set(nndf['Line_ID'].values)
     ldf = []
+    line = dict()
+
     for line_id in line_ids:
         sndf = ndf[ndf.Line_ID == line_id]
+        vehicle_ids = set(sndf['Vehicle_ID'].values)
+        # For a specific line_id, find the vehicle_id has largest data point
+        for vehicle_id in vehicle_ids:
+            tsndf = sndf[sndf.Vehicle_ID == vehicle_id]
+            if line_id in line.keys():
+                if line[line_id].shape[0] < tsndf.shape[0]:
+                    line[line_id] = tsndf
+            else:
+                line[line_id] = tsndf
+        # Make lines (from points)
         lsndf = sndf.copy()
         sndf.drop(sndf.index[-1],inplace=True)
         lsndf.drop(lsndf.index[0],inplace=True)
         sndf['toLon'] = lsndf['Lon'].values
         sndf['toLat'] = lsndf['Lat'].values
-        ldf.append(sndf)
+    # Combine all lines to a dataframe
+    for line_id in line.keys():
+        ldf.append(line[line_id])
     odf = pd.concat(ldf)
     output(odf,'route/',data_file)
 
+# Generate a json file contains all line_ids of all files
+def line2json(files):
+    lines = dict()
+    for data_file in files:
+        df = pd.read_csv(data_dir + data_file, header=None, names=header)
+        ndf = df[~df.Line_ID.isnull()]
+        line_ids = [str(i) for i in set(ndf['Line_ID'].values)]
+        lines[data_file] = line_ids
+    with open(data_dir + 'route/lines.txt','w') as f:
+        f.write(json.dumps(lines, indent=4))
 
 def main():
     files = os.listdir(data_dir)
     files = [item for item in files if (item[:4]=='siri')]
-    
+
+    line2json(files)
+
     for item in files:
         route(item)
         # stops(item)
@@ -78,4 +106,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
+
